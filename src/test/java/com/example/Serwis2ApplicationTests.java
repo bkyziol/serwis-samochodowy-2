16a8c6d139c504bc2e257e004380612b549d95fb
Bartosz Kyzioł <bkyziol@gmail.com>
2017-04-06 00:08:49 +0200

    Pierwszy commit

---
+++ src/test/java/com/example/Serwis2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Serwis2Application.class)
+public class Serwis2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
